c4e21c1d6daf45348e8276358fbbb10be9167500
WindOctober <windocotber@gmail.com>
2025-04-14 21:58:01 +0800

    Revise: plonkish package path -> Hyperplonk.

--- test_cli.sh
+++ test_cli.sh
@@ -12,7 +12,7 @@ test_default_cli() {
     cargo build --release
     # rm -rf params/*.data params/*.config output
     # $CLI --params ./params wasm_output setup --wasm ./fibonacci_wasm.wasm  $SCHEME -k 18
-    $CLI wasm_output prove --wasm crates/zkwasm/wasm/fibonacci.wasm --output ./output --public 5:i64
+    RUST_BACKTRACE=1 $CLI wasm_output prove --wasm crates/zkwasm/wasm/fibonacci.wasm --output ./output --public 5:i64
     # $CLI --params ./params wasm_output verify --output ./output
 }
 
